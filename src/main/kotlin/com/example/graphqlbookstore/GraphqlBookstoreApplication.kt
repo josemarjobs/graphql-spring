3a972bf0f435f8cc4bd379afe500e2c85e938b05
Josemar Magalhaes <josemarmagalhaes@hotmail.com>
2020-06-18 18:46:12 +0200

    bookstore: big bang

---
+++ src/main/kotlin/com/example/graphqlbookstore/GraphqlBookstoreApplication.kt
@@ -0,0 +1,11 @@
+package com.example.graphqlbookstore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphqlBookstoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<GraphqlBookstoreApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/graphqlbookstore/GraphqlBookstoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.graphqlbookstore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraphqlBookstoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
